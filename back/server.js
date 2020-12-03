b8e016196bd45289d1c4faf6a61f200d56c6a23f
jmillod <juancarlosmillord@gmail.com>
2020-12-02 20:03:18 -0400

    added middleware setup

--- back/server.js
+++ back/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
+const { ApolloServer, gql } = require("apollo-server-express");
+const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv");
+
+// set env variables
+
+dotenv.config();
+app.use(cors(cors));
+app.use(express.json());
 
 let port = 3001;
 
